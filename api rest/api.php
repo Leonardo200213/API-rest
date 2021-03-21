2601a3dbba10ef7a80e66d085ab1374cfe8ae5fc
Leonardo200213 <50982851+Leonardo200213@users.noreply.github.com>
2021-03-21 18:14:16 +0100

    aggiunto get, put e delete

--- api rest/api.php
+++ api rest/api.php
@@ -1,4 +1,5 @@
 <?php
+function fpost(){
 	$url = "http://localhost:8080/swagger-ui.html";
 	$data = ['collection' = > 'EsercizioAPI'];
 	$curl = curl_init($url);
@@ -8,7 +9,57 @@
 	curl_setopt($curl, CURLOPT_POSTFIELDS,  json_encode($data));
 	curl_setopt($curl, CURLOPT_HTTPHEADER, ['X-RapidAPI-Host: http://localhost:8080/swagger-ui.html', 'Content-Type: application/json']);
 
-	$risposta = curl_exec ($curl) ;
+	$response = curl_exec ($curl) ;
 	curl_close ($curl) ;
-	echo $risposta . PHP_EOL; 
+	echo $response . PHP_EOL; 
+}
+
+
+function fget(){
+	$url = 'http://localhost:8080/swagger-ui.html';
+	$collection_name = 'EsercizioAPI';
+	$request_url = $url . '/' . $collection_name;
+
+	$curl = curl_init($request_url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, ['X-RapidAPI-Host: http://localhost:8080/swagger-ui.html', 'Content-Type: application/json']);
+
+	$response = curl_exec($curl);
+	curl_close($curl);
+	echo $response . PHP_EOL;
+}
+
+
+function fput(){
+	$url = 'http://localhost:8080/swagger-ui.html';
+	$collection_name = 'EsercizioAPI';
+	$request_url = $url . '/' . $collection_name;
+
+	$data = ['public_write' => true];
+	$curl = curl_init($request_url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
+	curl_setopt($curl, CURLOPT_POSTFIELDS,  json_encode($data));
+	curl_setopt($curl, CURLOPT_HTTPHEADER, ['X-RapidAPI-Host: http://localhost:8080/swagger-ui.html', 'Content-Type: application/json']);
+	
+	$response = curl_exec($curl);
+	curl_close($curl);
+	echo $response . PHP_EOL;
+}
+
+
+function fdelete(){
+	$url = 'http://localhost:8080/swagger-ui.html';
+	$collection_name = 'EsercizioAPI';
+	$request_url = $url . '/' . $collection_name;
+
+	$curl = curl_init($request_url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
+	curl_setopt($curl, CURLOPT_HTTPHEADER, ['X-RapidAPI-Host: http://localhost:8080/swagger-ui.html', 'Content-Type: application/json']);
+	
+	$response = curl_exec($curl);
+	curl_close($curl);
+	echo $response . PHP_EOL;
+}
 ?>
